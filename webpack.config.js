66f2c164c0560e640ee15380a91775d493742bd3
mtbr39 <nm1552jp@gmail.com>
2024-01-16 14:21:30 +0900

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    // CopyWebpackPluginのオプションを修正
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public/index.html', to: 'index.html' },
+        // 他のコピー対象のファイルやディレクトリを追加できます
+      ],
+    }),
+  ],
+};
