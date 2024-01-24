f16114b0b71d9c5267d810e33bdcf9ea248e9781
mtbr39 <nm1552jp@gmail.com>
2024-01-24 14:30:30 +0900

    webpack minify build

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,42 @@
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+// const CopyWebpackPlugin = require("copy-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  mode: 'development', // モードを設定（developmentまたはproduction）
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
+    mode: "development", // モードを設定（developmentまたはproduction）
+    entry: "./src/index.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+        ],
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [{ from: "public/index.html", to: "index.html" }],
+        })
     ],
-  },
-  plugins: [
-    // CopyWebpackPluginのオプションを修正
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: 'public/index.html', to: 'index.html' },
-        // 他のコピー対象のファイルやディレクトリを追加できます
-      ],
-    }),
-  ],
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    format: {
+                        comments: false,
+                    },
+                },
+                extractComments: false,
+            }),
+        ],
+    },
 };
