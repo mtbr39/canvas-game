f9a6ce56691ceaf85543f971ab3ee94e408caac8
mtbr39 <nm1552jp@gmail.com>
2024-07-03 19:47:19 +0900

    UIObjectパラメータ

--- src/library/building/itemCollector.js
+++ src/library/building/itemCollector.js
@@ -3,6 +3,7 @@ import { GameObject } from "../../system/gameObject";
 import { DialogBox } from "../UI/DialogBox";
 import { UIManager } from "../UI/UIManager";
 import { ItemBag } from "../UI/itemBag";
+import { SingleButton } from "../UI/singleButton";
 import { SpriteAppearance } from "../appearance/spriteAppearance";
 import { FollowCamera } from "../module/followCamera";
 import { ClickMover } from "./clickMover";
@@ -46,6 +47,63 @@ export class ItemCollector {
         this.dialogBox = new DialogBox({ system: system });
         this.dialogBox.startTalk("scene01");
 
-        const uiManager = new UIManager({ system: system, itemBag: this.itemBag });
+        const uiManager = new UIManager({ system: system, itemBag: this.itemBag});
+        uiManager.UIObject = {
+            open: {
+                UIList: [
+                    new SingleButton({
+                        system: system,
+                        text: "Shop",
+                        alignment: {typeX: "right", typeY: "top", top: 20, right: 20},
+                        size: { width: 100, height: 32 },
+                        isDisplay: true,
+                        handler: () => { uiManager.switchDisplayPage('shopHome', 'open') },
+                    }),
+                ],
+            },
+            shopHome: {
+                UIList: [
+                    new SingleButton({
+                        system: system,
+                        text: "Buy",
+                        alignment: {typeX: "right", typeY: "top", top: 20, right: 20},
+                        size: { width: 100, height: 32 },
+                        handler: () => {},
+                    }),
+                    new SingleButton({
+                        system: system,
+                        text: "Sell",
+                        alignment: {typeX: "right", typeY: "top", top: 60, right: 20},
+                        size: { width: 100, height: 32 },
+                        handler: () => {},
+                    }),
+                    new SingleButton({
+                        system: system,
+                        text: "Back",
+                        alignment: {typeX: "right", typeY: "top", top: 100, right: 20},
+                        size: { width: 100, height: 32 },
+                        handler: () => { uiManager.switchDisplayPage('open', 'shopHome') },
+                    }),
+                ],
+            },
+        };
+        const bagButton = new SingleButton({
+            system: system,
+            text: "ItemBag",
+            alignment: {typeX: "left", typeY: "top", top: 20, left: 20},
+            size: { width: 100, height: 32 },
+            handler: () => {uiManager.switchDisplayPage('bagContent', 'bagOpen')},
+            isDisplay: true
+        });
+        const bagCloseButton = new SingleButton({
+            system: system,
+            text: "Close ItemBag",
+            alignment: {typeX: "left", typeY: "top", top: 20, left: 20},
+            size: { width: 100, height: 32 },
+            handler: () => {uiManager.switchDisplayPage('bagOpen', 'bagContent')},
+        });
+        uiManager.UIObject['bagOpen'] = {UIList: [bagButton]};
+        uiManager.UIObject['bagContent'] = {UIList: [bagCloseButton, this.itemBag]};
+        
     }
 }
