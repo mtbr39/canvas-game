5482b6a4485897d595019d65788c8b2b41009d19
mtbr39 <nm1552jp@gmail.com>
2024-07-03 19:57:06 +0900

    launcherをapplicationフォルダに移行

---
+++ src/application/street.js
@@ -0,0 +1,33 @@
+import { assets } from "../const/assets";
+
+import { StreetHuman } from "../library/street/streetHuman";
+import { StreetPath } from "../library/street/streetPath";
+import { StreetGenerator } from "../library/street/streetGenerator";
+import { PlaceManager } from "../library/street/placeManager";
+
+export default (reson) => {
+    console.log("v418");
+    const Reson = reson;
+    const systemList = Reson.systemList;
+
+    Reson.cameraSystem.turnOnDrag = true;
+    systemList.render.setBackGroundColor("#acdcaa");
+    systemList.drawer.initialLoadImages(assets);
+
+    const streetPath = new StreetPath({system: systemList});
+
+    const placeManager = new PlaceManager({system: systemList});
+
+    const streetGenerator = new StreetGenerator({system: systemList, streetPath: streetPath, placeManager: placeManager});
+
+    for (let i=0; i < 1; i++) {
+        new StreetHuman({systemList: systemList, streetPath: streetPath, placeManager: placeManager});
+    }
+
+    const animalFactory = Reson.animalFactory;
+    // animalFactory.hasElevation = true;
+    // animalFactory.make({ number: 50, x: 200, y: 400,layers: ["animal"], speciesName: "boidA", shapeColor: "gray", hasNotWall: true });
+    // animalFactory.make({ number: 30, x: 200, y: 400,layers: ["animal"], speciesName: "boidB", shapeColor: "gray", hasNotWall: true, width: 15, height: 15, velocity: 0.4, });
+    
+    
+}
--- src/launcher.js
+++ src/launcher.js
@@ -10,6 +10,7 @@ import { StreetGenerator } from "./library/street/streetGenerator";
 import { PlaceManager } from "./library/street/placeManager";
 import { DropItem } from "./library/building/dropItem";
 import Ensemble from './library/ensemble/ensembleMain';
+import Street from './application/street';
 
 export class Launcher {
     constructor(option={}) {
@@ -31,7 +32,7 @@ export class Launcher {
             this.canvas2d();
         }
         if (this.projectID === "routeman") {
-            this.routeman();
+            Street(this.reson);
         }
         if (this.projectID === "ensemble") {
             Ensemble.init(this.reson);
@@ -81,30 +82,5 @@ export class Launcher {
         }
         
     }
-
-    routeman() {
-        console.log("v418");
-        const systemList = this.reson.systemList;
-
-        this.reson.cameraSystem.turnOnDrag = true;
-        systemList.render.setBackGroundColor("#acdcaa");
-        systemList.drawer.initialLoadImages(assets);
-
-        const streetPath = new StreetPath({system: systemList});
-
-        const placeManager = new PlaceManager({system: systemList});
-
-        const streetGenerator = new StreetGenerator({system: systemList, streetPath: streetPath, placeManager: placeManager});
-
-        for (let i=0; i < 1; i++) {
-            new StreetHuman({systemList: systemList, streetPath: streetPath, placeManager: placeManager});
-        }
-
-        const animalFactory = this.reson.animalFactory;
-        // animalFactory.hasElevation = true;
-        // animalFactory.make({ number: 50, x: 200, y: 400,layers: ["animal"], speciesName: "boidA", shapeColor: "gray", hasNotWall: true });
-        // animalFactory.make({ number: 30, x: 200, y: 400,layers: ["animal"], speciesName: "boidB", shapeColor: "gray", hasNotWall: true, width: 15, height: 15, velocity: 0.4, });
-        
-        
-    }
+    
 }
